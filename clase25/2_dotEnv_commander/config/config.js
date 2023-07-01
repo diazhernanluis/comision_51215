3c857ba2982aaf2f1e78c95bbf29c94da1d06b74
Hernan Diaz <diazher59@gmail.com>
2023-07-01 16:20:52 -0300

    fix dotEnv + commander

--- clase25/2_dotEnv_commander/config/config.js
+++ clase25/2_dotEnv_commander/config/config.js
@@ -1,3 +1,23 @@
+import dotenv from 'dotenv';
+import { Command } from 'commander';
+import __dirname from '../dirname.js';
+
+const program = new Command();
+
+program
+.option('--name', 'nombre del ambiente')
+.requiredOption('--mode <item>', 'ambiente de trabajo actual');
+
+program.parse();
+
+let options = program.opts();
+console.log("MODE: ", options.mode);
+
+if(options.mode === "d") {
+    dotenv.config({path: __dirname + '/.env.development'});
+} else if (options.mode === "p") {
+    dotenv.config({path: __dirname + '/.env.production'});
+}
 
 let config = {};
 
--- clase25/2_dotEnv_commander/index.js
+++ clase25/2_dotEnv_commander/index.js
@@ -3,40 +3,20 @@
 // (--mode development debería conectar con .env.development).
 // Para el entorno development, el servidor debe escuchar en el puerto 8080, para el entorno productivo, el servidor debe escuchar en el puerto 3000. 
 
-import dotenv from 'dotenv';
+
 import express from 'express';
-import { Command } from 'commander';
-import __dirname from './dirname.js';
-import path from 'path';
-
-const program = new Command();
-
-program
-    .option('--name', 'nombre del ambiente')
-    .requiredOption('--mode <item>', 'ambiente de trabajo actual');
-
-program.parse();
-
-let options = program.opts();
-console.log("MODE: ", options.mode);
-
-if(options.mode === "d") {
-    dotenv.config({path: path.join(__dirname + './.env.development')});
-    //require("dotenv").config({path: './.env.production'});
-} else if (options.mode === "p") {
-    dotenv.config({path: './.env.production'});
-    //require("dotenv").config({path: './.env.production'});
-}
-
 import config from './config/config.js';
+import __dirname from './dirname.js';
 
+console.log(config);
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.status(200).send(` <h1> PUERTO: ${config.server.port} </h1> </br><h1> MONGO: ${config.server.MONGO_URL} </h1>`);
-})
+});
+
 app.listen(config.server.port, () => {
     console.log(`Server running on PORT: ${config.server.port}`);
-})
+});
