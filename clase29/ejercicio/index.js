1bceb404331a0c26e1fa28354055e1088be8c3c9
Hernan Diaz <diazher59@gmail.com>
2023-07-18 19:22:40 -0300

    clase 29 - faltante

--- clase29/ejercicio/index.js
+++ clase29/ejercicio/index.js
@@ -2,12 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import indexRouter from './routes/index.routes.js';
 import config from './config/config.js';
+import cors from 'cors';
+
 const PORT = config.server;
 
 const app = new express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(indexRouter);
 
 app.listen(PORT, () => {
